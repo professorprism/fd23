fda2d1834cfbbab42a293ffb330c976a1c1566be
professorprism <bkoster@usc.edu>
2025-02-15 14:58:26 -0800

    first commit

---
+++ lib/f_d_state.dart
@@ -0,0 +1,67 @@
+
+import 'dart:convert';
+// import "package:flutter_bloc/flutter_bloc.dart";
+
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+// import 'package:path_provider/path_provider.dart';
+import 'package:equatable/equatable.dart';
+
+// Note: the example has NAMES paramters in the constructor.
+// We could switch to that at some point.
+
+class FDState  extends Equatable
+{ 
+  final String food;
+
+  FDState(this.food);
+
+  @override
+  List<Object?> get props => [food];
+
+  FDState copyWith({ String? food, }) 
+    => FDState( food ?? this.food, );
+
+
+  // turns the object into a map
+  Map<String,dynamic> toMap()
+  { return
+    { 'food' : food ,
+    };
+  } 
+  
+  // turn a map back into an object
+  factory FDState.fromMap(Map<String,dynamic> map)
+  {
+    return FDState( map['food'] );
+  }
+
+  // turns the object into JSON.  Does this by 
+  // call toMap and then encode() ing the map.
+  String toJson() => json.encode(toMap);
+
+  // turns Json back into an objevct.  
+  factory FDState.fromJson( String source) 
+  => FDState.fromMap( json.decode(source));
+   
+}
+
+class FDCubit extends HydratedCubit<FDState> // with HydratedMixin
+{
+  FDCubit() : super( FDState("banana") );
+
+  void setFood(String f ) { emit( FDState(f) ); }
+
+  // converts the map form of FDState into an object.
+  // should have been called fromMap, as the Hydrated stuff
+  // will have already converted it from JSON to a map.
+  @override
+  FDState fromJson( Map<String,dynamic> map)
+  { return FDState.fromMap(map); }
+
+  // This is called on s AFTER emit(s).  Every time there is a new
+  // state, this function converts it to a Map and the Hydrated
+  // stuff takes it from there.  
+  @override
+  Map<String,dynamic> toJson( FDState state )
+  { return state.toMap(); }
+}
---
+++ lib/oldfdmaybe.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'f_d_state.dart';
+
+void main() async
+{ WidgetsFlutterBinding.ensureInitialized();
+  var add = await getApplicationDocumentsDirectory();
+  //HydratedBloc.storage = await HydratedStorage.build
+  //( storageDirectory:  HydratedStorage(add).path );
+  // print("add="+add.toString());
+  runApp(const FoodDiary());
+}
+
+class FoodDiary extends StatelessWidget
+{ static const String header = "Food Diary 4";
+
+  const FoodDiary({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  { return MaterialApp
+    ( title: header,
+      home: BlocProvider<FDCubit>
+      ( create: (context) => FDCubit(),
+        child: BlocBuilder<FDCubit,FDState>
+        ( builder: (context,state)
+          { FDCubit fdc =  BlocProvider.of<FDCubit>(context);
+            return  Splash(title: header, fdc:fdc);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Splash extends StatelessWidget 
+{ final String title;
+  final FDCubit fdc;
+  const Splash({super.key, required this.title, required this.fdc});
+
+  @override
+  Widget build(BuildContext context) 
+  { FDCubit fdc = BlocProvider.of<FDCubit>(context);
+    FDState fds = fdc.state;
+    TextEditingController tec = TextEditingController();
+
+    return Scaffold
+    ( appBar: AppBar(  title: Text(title),),
+      body: Column
+      ( children: 
+        [ Text(fds.food),
+          SizedBox
+          ( height:50, width:300,
+            child: TextField(controller: tec ),
+          ),
+          ElevatedButton
+          ( onPressed: (){ fdc.setFood(tec.text); },
+            child: Text("submit"),
+          ),
+        ],
+      ),
+    );
+  }     
+}
